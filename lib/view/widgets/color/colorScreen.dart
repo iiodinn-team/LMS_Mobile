9fa49c8632ab02815b1bb62328845a8058ab5901
leang bunleap <leangbunleap168@gmail.com>
2024-11-25 15:05:55 +0700

    add colors

--- lib/view/widgets/color/colorScreen.dart
+++ lib/view/widgets/color/colorScreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF253B95); // Primary color
-  static const Color secondaryColor = Color(0xFFF7E5E5); // Example secondary color
-  static const Color textColor = Color(0xFF333333); // Example text color
-  static const Color backgroundColor = Color(0xFFF5F5F5); // Example background color
+  static const Color primaryColor = Color(0xFF253B95);
+  static const Color secondaryColor = Color(0xFFF63030);
+  static const Color accentColor = Color(0xFFEAB305);
+  static const Color defaultBlackColor = Color(0xFF000000);
+  static const Color defaultWhitColor = Color(0xFFFFFFFF);
+  static const Color defaultGrayColor = Color(0xFF535763);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
 }
