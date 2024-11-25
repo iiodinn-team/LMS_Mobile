6fd8beeed6df64f9b4d2c56b0ecca7b5be9d1828
leang bunleap <leangbunleap168@gmail.com>
2024-11-25 14:26:35 +0700

    first commit

---
+++ lib/view/widgets/color/colorScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF253B95); // Primary color
+  static const Color secondaryColor = Color(0xFFF7E5E5); // Example secondary color
+  static const Color textColor = Color(0xFF333333); // Example text color
+  static const Color backgroundColor = Color(0xFFF5F5F5); // Example background color
+}
